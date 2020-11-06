44d8603d76c19232eaeee4de8d3c669e243b5638
SeuraG <alexanderseur@gmail.com>
2020-11-06 16:32:51 -0300

    Version de Control de PokeMaps actividad 2.5 se agrego una alerta de permisos de gps.

--- app/src/main/java/com/example/pokemap/Principal.java
+++ app/src/main/java/com/example/pokemap/Principal.java
@@ -1,7 +1,9 @@
 package com.example.pokemap;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.hardware.Sensor;
@@ -9,6 +11,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,6 +22,7 @@ public class Principal extends AppCompatActivity {
     SensorManager sensorManager;
     Sensor sensor;
     SensorEventListener sensorEventListener;
+    AlertDialog alerta = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,8 @@ public class Principal extends AppCompatActivity {
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+
 
         if (sensor == null)
             finish();
@@ -69,8 +75,30 @@ public class Principal extends AppCompatActivity {
             }
         }
         );
+        getAlertaNotGps();
         }
-        public void start(){
+
+    private void getAlertaNotGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Por favor encienda el GPS. Desea activarlo?")
+                .setCancelable(false)
+                .setTitle("GPS")
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@SuppressWarnings("unused")final DialogInterface dialog, @SuppressWarnings("unused")final int id) {
+                        startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick( final DialogInterface dialog, @SuppressWarnings("unused")final int id) {
+                dialog.cancel();
+            }
+        });
+        alerta = builder.create();
+        alerta.show();
+    }
+
+    public void start(){
             sensorManager.registerListener(sensorEventListener,sensor,2000*1000);
         }
         public  void stop(){
